1221881064f777f291e5bef99e398c802bf22056
satokofuwa <96944825+satokofuwa@users.noreply.github.com>
2022-03-03 18:01:26 +0900

    routes作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    resources :posts do
+      collection do
+        post :confirm
+      end
+      member do
+        patch :confirm
+      end
+    end
+
+  
 end
+
